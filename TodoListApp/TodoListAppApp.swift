4256ec7c2d9b2db2a6556d2c4a014068409be793
iamsadcopy <iamsadcopy@gmail.com>
2022-01-15 04:04:15 +0300

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Pavel Moskvichev on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
